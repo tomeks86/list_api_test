71e200b8db95bdd083149f5cc81ff827dc9124f6
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-04-11 16:25:02 +0200

    AL: size

---
+++ src/edu/tseidler/TestArrayList.java
@@ -0,0 +1,47 @@
+package edu.tseidler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestArrayList {
+    public static void shouldGiveZeroSize() {
+        // given
+        List test = new ArrayList();
+
+        // when
+
+
+        // then
+        assert test.size() == 0 : "non-zero size!";
+    }
+
+    public static void shouldGiveSizeOfOne() {
+        // given
+        List test = new ArrayList();
+
+        // when
+        test.add(new Object());
+
+        // then
+        assert test.size() == 1 : "size should be 1!";
+    }
+
+    public static void shouldGiveZeroSizeAfterAdditionAndRemoval() {
+        // given
+        List test = new ArrayList();
+
+        // when
+        test.add(new Object());
+        test.remove(0);
+
+        // then
+        assert test.size() == 0 : "size should be again 0!";
+    }
+
+    public static void main(String[] args) {
+        // size
+        shouldGiveZeroSize();
+        shouldGiveSizeOfOne();
+        shouldGiveZeroSizeAfterAdditionAndRemoval();
+    }
+}
